c8c197045e5817e1ae8a352214e4b9dec55c97c3
Rusydy <rusydymuhiddin@gmail.com>
2023-09-04 23:07:30 +0700

    annalyns-infiltration challenge

---
+++ annalyns-infiltration/AnnalynsInfiltration.cs
@@ -0,0 +1,36 @@
+using System;
+
+using System;
+
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+        return !knightIsAwake;
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+         return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+        if (archerIsAwake || !prisonerIsAwake)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        if ((petDogIsPresent && !archerIsAwake) || (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
